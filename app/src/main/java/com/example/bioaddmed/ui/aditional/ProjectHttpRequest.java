087166f274750d6e29f6fc12de83cb84b16ff54b
Damian <damianpietron007@gmail.com>
2024-01-26 15:51:49 +0100

    Add floating button at start of a program

--- app/src/main/java/com/example/bioaddmed/ui/aditional/ProjectHttpRequest.java
+++ app/src/main/java/com/example/bioaddmed/ui/aditional/ProjectHttpRequest.java
@@ -1,4 +1,64 @@
-package com.example.bioaddmed.ui.aditional;
-
-public class ProjectHttpRequest {
-}
+//package com.example.bioaddmed.ui.aditional;
+//
+//import android.os.AsyncTask;
+//import android.util.Log;
+//
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//
+//import kotlinx.coroutines.JobNode;
+//
+//public class ProjectHttpRequest extends AsyncTask<String, Void, String> {
+//    private OnTaskCompleted listener;
+//
+//    public ProjectHttpRequest(OnTaskCompleted listener) {
+//        this.listener = listener;
+//    }
+//
+//    @Override
+//    protected String doInBackground(String... strings) {
+//        StringBuilder stringBuilder = new StringBuilder();
+//        try {
+//            URL url = new URL(strings[0]);
+//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//            connection.setRequestMethod("GET");
+//            connection.setDoInput(true);
+//            connection.setRequestProperty("password", "B10@dDM3d1229");
+//            connection.connect();
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//            String line;
+//
+//            while ((line = reader.readLine()) != null) {
+//                stringBuilder.append(line);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//        return stringBuilder.toString();
+//    }
+//
+//    @Override
+//    protected void onPostExecute(JSONObject result) {
+//        super.onPostExecute(result);
+//        String quote;
+//        try {
+//            JSONObject jsonObject = new JSONObject(result);
+//            quote = jsonObject.getJSONObject("data").getString("project");
+//            listener.onTaskCompleted(quote); // Pass the quote to the listener
+//        } catch (JSONException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    public interface OnTaskCompleted {
+//        void onTaskCompleted(String quote);
+//    }
+//
+//}
--- app/src/main/java/com/example/bioaddmed/ui/aditional/StartingActivity.kt
+++ app/src/main/java/com/example/bioaddmed/ui/aditional/StartingActivity.kt
@@ -3,22 +3,29 @@ package com.example.bioaddmed.ui.aditional
 import android.annotation.SuppressLint
 import android.app.UiModeManager
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.bioaddmed.R
 import com.example.bioaddmed.ui.user.LogInActivity
 import com.example.bioaddmed.ui.user.RegisterActivity
 
 class StartingActivity : AppCompatActivity() {
 
-
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_starting)
 
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.mainLayout)
+
+        val animationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2000)
+        animationDrawable.setExitFadeDuration(2000)
+        animationDrawable.start()
 
         val registerButton: Button = findViewById(R.id.Register)
         registerButton.setOnClickListener {
@@ -33,6 +40,7 @@ class StartingActivity : AppCompatActivity() {
             startActivity(intent)
             finish();
         }
+
         val textView = findViewById<android.widget.TextView>(R.id.Quote)
         val httpRequest = CiteHttpRequest(object : CiteHttpRequest.OnTaskCompleted {
             override fun onTaskCompleted(quote: String) {
@@ -46,3 +54,4 @@ class StartingActivity : AppCompatActivity() {
         uiModeManager?.nightMode = UiModeManager.MODE_NIGHT_YES
     }
 }
+
